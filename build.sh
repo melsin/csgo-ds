7f97635fb9ecf5dadb15ff37942b4643278fcdb9
Simon Dawson <simon.dawson@sas.com>
2020-04-27 15:38:15 +1000

    Fix up retakes names. Its retakeS not retake.

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ docker build -t steamcmd dockerfiles/steamcmd
 docker build -t csgo-ds dockerfiles/csgo-ds
 docker build -t metamod dockerfiles/metamod
 docker build -t sourcemod dockerfiles/sourcemod
+docker build -t retakes dockerfiles/retakes
--- download-assets.sh
+++ download-assets.sh
@@ -9,13 +9,13 @@ mkdir -p dockerfiles/sourcemod/assets
 curl -o dockerfiles/sourcemod/assets/sourcemod-1.11.0-git6526-linux.tar.gz https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6526-linux.tar.gz
 
 ## Retake SourceMod plugin
-mkdir -p dockerfiles/retake/assets
-curl -L -o dockerfiles/retake/assets/retakes_0.3.4.zip https://github.com/splewis/csgo-retakes/releases/download/v0.3.4/retakes_0.3.4.zip
-mkdir dockerfiles/retake/assets/unpack
-unzip -q dockerfiles/retake/assets/retakes_0.3.4.zip -d dockerfiles/retake/assets/unpack
-cd dockerfiles/retake/assets/unpack
+mkdir -p dockerfiles/retakes/assets
+curl -L -o dockerfiles/retakes/assets/retakes_0.3.4.zip https://github.com/splewis/csgo-retakes/releases/download/v0.3.4/retakes_0.3.4.zip
+mkdir dockerfiles/retakes/assets/unpack
+unzip -q dockerfiles/retakes/assets/retakes_0.3.4.zip -d dockerfiles/retake/assets/unpack
+cd dockerfiles/retakes/assets/unpack
 tar -czf retakes_0.3.4.tar.gz *
 mv retakes_0.3.4.tar.gz ../
 cd ../../../../
-rm -r dockerfiles/retake/assets/unpack
+rm -r dockerfiles/retakes/assets/unpack
 
