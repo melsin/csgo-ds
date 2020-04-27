b2693090d2a1cbf31aa9db0deaf49f738b393862
Simon Dawson <31032759+sas-simon@users.noreply.github.com>
2020-04-27 18:13:17 +1000

    Missed one s

--- download-assets.sh
+++ download-assets.sh
@@ -12,7 +12,7 @@ curl -o dockerfiles/sourcemod/assets/sourcemod-1.11.0-git6526-linux.tar.gz https
 mkdir -p dockerfiles/retakes/assets
 curl -L -o dockerfiles/retakes/assets/retakes_0.3.4.zip https://github.com/splewis/csgo-retakes/releases/download/v0.3.4/retakes_0.3.4.zip
 mkdir dockerfiles/retakes/assets/unpack
-unzip -q dockerfiles/retakes/assets/retakes_0.3.4.zip -d dockerfiles/retake/assets/unpack
+unzip -q dockerfiles/retakes/assets/retakes_0.3.4.zip -d dockerfiles/retakes/assets/unpack
 cd dockerfiles/retakes/assets/unpack
 tar -czf retakes_0.3.4.tar.gz *
 mv retakes_0.3.4.tar.gz ../
