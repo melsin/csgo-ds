3e3947e60ac48b4fdd188e218d31305f46d02440
Simon Dawson <simon.dawson@sas.com>
2020-04-28 13:50:46 +1000

    ADD directive in a Dockerfile can download a URL, no need to stage the tar.gz files before hand

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+
 docker build -t steamcmd dockerfiles/steamcmd
-docker build -t csgo-ds dockerfiles/csgo-ds
-docker build -t metamod dockerfiles/metamod
-docker build -t sourcemod dockerfiles/sourcemod
+docker build -t srcds-csgo dockerfiles/csgo-ds
+docker build -t metamod-csgo dockerfiles/metamod
+docker build -t sourcemod-csgo dockerfiles/sourcemod
 docker build -t retakes dockerfiles/retakes
--- download-assets.sh
+++ download-assets.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-## Metamod Source
-mkdir -p dockerfiles/metamod/assets
-curl -o dockerfiles/metamod/assets/mmsource-1.11.0-git1130-linux.tar.gz https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1130-linux.tar.gz
-
-## SourceMod
-mkdir -p dockerfiles/sourcemod/assets
-curl -o dockerfiles/sourcemod/assets/sourcemod-1.11.0-git6526-linux.tar.gz https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6526-linux.tar.gz
-
 ## Retake SourceMod plugin
 mkdir -p dockerfiles/retakes/assets
 curl -L -o dockerfiles/retakes/assets/retakes_0.3.4.zip https://github.com/splewis/csgo-retakes/releases/download/v0.3.4/retakes_0.3.4.zip
