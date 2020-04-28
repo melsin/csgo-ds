7ea4bfabeb87febaa1ed5513b5d5283b93128a4c
Simon Dawson <simon.dawson@sas.com>
2020-04-28 14:22:48 +1000

    Remove the old build.sh since I've moved to docker-compose for builds

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker build -t steamcmd dockerfiles/steamcmd
-docker build -t srcds-csgo dockerfiles/csgo-ds
-docker build -t metamod-csgo dockerfiles/metamod
-docker build -t sourcemod-csgo dockerfiles/sourcemod
-docker build -t retakes dockerfiles/retakes
